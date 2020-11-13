123bd8d053832458e950966ec716ad284d2430e9
Margarita <mivillafanep@unincca.edu.co>
2020-11-12 23:53:56 -0500

    marvil.ext - add Header at methods

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set('display_errors', 1);
 error_reporting(-1);
@@ -9,23 +9,30 @@ require_once 'kiosco.php';
 $objKiosco = new kiosco();
 $vUri = explode("api.php/", $_SERVER['REQUEST_URI']);
 $cMetodo = $_SERVER['REQUEST_METHOD'];
-$mDatos = ["message" => "", "data" => []];
+$mDatos = ["resultado" => "", "data" => []];
 switch ($cMetodo) {
   case "POST":
+    header('Content-Type: application/json');
     $mParams = json_decode(file_get_contents('php://input'), true);
     $mResult = $objKiosco->{$vUri[1]}($mParams);
     break;
   case "GET":
     $mResult = $objKiosco->{$vUri[1]}();
     break;
+  case "OPTIONS":
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header("Access-Control-Allow-Headers: Content-Type, origin");
+    header('Access-Control-Max-Age: 86400');
+    die(0);
+    break;
   default:
     echo "Metodo invalido.";
     break;
 }
 if (!empty($mResult)) {
-  $mDatos["message"] = "Consulta Exitosa";
+  $mDatos["resultado"] = "Consulta Exitosa";
   $mDatos["data"] = $mResult;
 } else {
-  $mDatos["message"] = "Sin Resultados";
+  $mDatos["resultado"] = "Sin Resultados";
 }
 echo json_encode($mDatos);
--- database.php
+++ database.php
@@ -1,21 +1,24 @@
 <?php
 
-class database {
+class database
+{
   private $host;
   private $db;
   private $user;
   private $password;
   private $charset;
 
-  public function __construct() {
-    $this->host = '';
-    $this->db = 'KIOSCO';
-    $this->user = '';
-    $this->password = "";
+  public function __construct()
+  {
+    $this->host = '127.0.0.1';
+    $this->db = 'kiosco';
+    $this->user = 'root';
+    $this->password = "root";
     $this->charset = 'utf8';
   }
 
-  function connect() {
+  function connect()
+  {
     try {
       $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
       $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false,];
